6986c8df934affbeee4bb05db5a5963bddb72f26
murther <stevedelgado93@gmail.com>
2014-11-20 17:28:37 -0800

    Update FriendList.java

--- app/src/main/java/com/steve/basicproject/FriendList.java
+++ app/src/main/java/com/steve/basicproject/FriendList.java
@@ -1,13 +1,17 @@
 package com.steve.basicproject;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,63 +21,44 @@ import java.util.List;
 /**
  * Created by Steve on 10/23/2014.
  */
-public class FriendList extends Activity {
-    private List<Friend> myFriends = new ArrayList<Friend>();
+public class FriendList extends ListActivity{
+    @Override
+    public long getSelectedItemId() {
+        // TODO Auto-generated method stub
+        return super.getSelectedItemId();
+    }
 
     @Override
-    public void onCreate(Bundle savedInstance){
-        super.onCreate(savedInstance);
-        setContentView(R.layout.friendlist);
-        setMyFriends();
-        setFriendListView();
+    public int getSelectedItemPosition() {
+        // TODO Auto-generated method stub
+        return super.getSelectedItemPosition();
     }
-    private void setMyFriends(){
-        myFriends.add(new Friend("Friend 1",1,"Favorite",R.drawable.myspace_tom));
-        myFriends.add(new Friend("Friend 2",2,"Regular",R.drawable.myspace_tom));
-        myFriends.add(new Friend("Friend 3",3,"Regular",R.drawable.myspace_tom));
-        myFriends.add(new Friend("Friend 4",4,"Favorite",R.drawable.myspace_tom));
-        myFriends.add(new Friend("Friend 5",5,"Favorite",R.drawable.myspace_tom));
-        myFriends.add(new Friend("Friend 6",6,"Favorite",R.drawable.myspace_tom));
-    }
-    private void setFriendListView(){
-        ArrayAdapter<Friend> adapter = new MyListAdapter();
-        ListView list= (ListView)findViewById(R.id.list);
-        list.setAdapter(adapter);
-    }
-    /*private void registerClickCallback(){
-        ListView list=  (ListView)findViewById(R.id.list);
-        list.setOnClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-        public void onItemClick(AdapterView<?> parent, View viewClicked,int position, long id){
-                Friend clickedFriend = myFriends.get(position);
-                String message="you clicked"+clickedFriend.getPerson();
-                Toast.makeText(FriendList.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
-    } */
-    private class MyListAdapter extends ArrayAdapter<Friend>{
-        public MyListAdapter(){
-            super(FriendList.this,R.layout.friendlistimages,myFriends);
-        }
+
+    ListView lv;
+    Cursor cursor1;
+
+
     @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-        View itemView= convertView;
-        if(itemView==null){
-            itemView=getLayoutInflater().inflate(R.layout.friendlistimages,parent, false);
-        }
-        //find friend
-        Friend currentFriend = myFriends.get(position);
-        //fill in the list for the view
-        ImageView imageView= (ImageView)findViewById(R.id.image1);
-        imageView.setImageResource(currentFriend.getIconId());
-        //friend Name
-        TextView friendText=(TextView)itemView.findViewById(R.id.friendName);
-        friendText.setText(currentFriend.getPerson());
-        //status
-        TextView statusText=(TextView)itemView.findViewById(R.id.friendStatus);
-        statusText.setText(currentFriend.getFavorite());
-        return itemView;
-        //return super.getView(position, convertView,parent);
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friendlist);
+
+        // create a cursor to query the Contacts on the device to start populating a listview
+        cursor1 = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        startManagingCursor(cursor1);
+
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID}; // get the list items for the listadapter could be TITLE or URI
+
+
+        int[] to = {android.R.id.text1, android.R.id.text2}; // sets the items from above string to listview
+
+        // new listadapter, created to use android checked template
+        SimpleCursorAdapter listadapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor1, from, to);
+        setListAdapter(listadapter);
+
+        // adds listview so I can get data from it
+        lv = getListView();
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
     }
 }
